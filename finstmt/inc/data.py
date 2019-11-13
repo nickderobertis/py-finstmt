931f7a46ecfaf1c1744be18310e056203de05da0
Nick DeRobertis <whoopnip@gmail.com>
2019-11-13 13:34:47 -0500

    Add data class for income statement

---
+++ finstmt/inc/data.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass(unsafe_hash=True)
+class IncomeStatementData:
+    revenue: float
+    cogs: float
+    rd_exp: float
+    sga: float
+    int_exp: float
+    tax_exp: float
+
+    op_exp: Optional[float] = None
+
+    def __post_init__(self):
+        if self.op_exp is None:
+            self.op_exp = self.rd_exp + self.sga  # TODO: check
+
+    @property
+    def gross_profit(self) -> float:
+        return self.revenue - self.cogs
+
+    @property
+    def ebit(self) -> float:
+        return self.gross_profit - self.op_exp
+
+    @property
+    def ebt(self) -> float:
+        return self.ebit - self.int_exp
+
+    @property
+    def net_income(self) -> float:
+        return self.ebt - self.tax_exp
+
+    @property
+    def effective_tax_rate(self) -> float:
+        return self.tax_exp / self.ebt
+
