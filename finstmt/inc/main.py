5c3d5a2a5b9dd2f8a35e90d01c53b14b93eb9ed6
Nick DeRobertis <whoopnip@gmail.com>
2019-11-13 18:12:22 -0500

    Restructure df handling into base class to prepare for balance sheet

--- finstmt/inc/main.py
+++ finstmt/inc/main.py
@@ -1,41 +1,14 @@
-import datetime
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import Dict
 
 import pandas as pd
 
+from finstmt.findata.statementsbase import FinStatementsBase
 from finstmt.inc.data import IncomeStatementData
 
 
 @dataclass
-class IncomeStatements:
+class IncomeStatements(FinStatementsBase):
     statements: Dict[pd.Timestamp, IncomeStatementData]
 
-    def __post_init__(self):
-        self.df = self.to_df()
-
-    def _repr_html_(self):
-        return self._formatted_df._repr_html_()
-
-    @classmethod
-    def from_df(cls, df: pd.DataFrame):
-        statements_dict = {}
-        for col in df.columns:
-            statement = IncomeStatementData.from_series(df[col])
-            statement_date = pd.to_datetime(col)
-            statements_dict[statement_date] = statement
-        return cls(statements_dict)
-
-    def to_df(self) -> pd.DataFrame:
-        all_series = []
-        for date, statement in self.statements.items():
-            series = statement.to_series()
-            series.name = date
-            all_series.append(series)
-        return pd.concat(all_series, axis=1)
-
-    @property
-    def _formatted_df(self) -> pd.DataFrame:
-        out_df = self.df.copy()
-        out_df.columns = [col.strftime('%m/%d/%Y') for col in out_df.columns]
-        return out_df.applymap(lambda x: f'${x:,.0f}')
+    statement_cls = IncomeStatementData
