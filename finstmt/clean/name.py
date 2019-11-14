36ae2aa3b5b6832a53e5b40b9c1782ee82a351e5
Nick DeRobertis <whoopnip@gmail.com>
2019-11-13 19:00:14 -0500

    Ensure only a single space between words in extracted names

--- finstmt/clean/name.py
+++ finstmt/clean/name.py
@@ -21,4 +21,5 @@ def standardize_name_for_look_up(name: str) -> str:
     name = name.lower().strip()
     name = ' '.join(name.split('_'))
     name = name.translate(str.maketrans('', '', string.punctuation))  # remove punctuation
+    name = ' '.join([part for part in name.split(' ') if part])  # ensure there is only a single space between words
     return name
