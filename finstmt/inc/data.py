59ed9a0974f221c20d02678fe01c00d2fa269694
Nick DeRobertis <whoopnip@gmail.com>
2019-11-14 05:48:37 -0500

    Fill out income statement data. Get more items instead of calculating if they are there.

--- finstmt/inc/data.py
+++ finstmt/inc/data.py
@@ -7,36 +7,36 @@ from finstmt.inc.config import INCOME_STATEMENT_INPUT_ITEMS
 
 @dataclass(unsafe_hash=True)
 class IncomeStatementData(FinDataBase):
-    revenue: float
+    revenue: float  # TODO: get total revenue first, then revenue if it's not there
     cogs: float
-    rd_exp: float
     sga: float
     int_exp: float
     tax_exp: float
-    # TODO: there's no depreciation in the stockrow data
+
+    rd_exp: Optional[float] = 0
+    dep_exp: Optional[float] = 0
+    other_op_exp: Optional[float] = 0
 
     op_exp: Optional[float] = None
+    ebit: Optional[float] = None
+    ebt: Optional[float] = None
+    net_income: Optional[float] = None
+
     items_config = INCOME_STATEMENT_INPUT_ITEMS
 
     def __post_init__(self):
         if self.op_exp is None:
-            self.op_exp = self.rd_exp + self.sga
+            self.op_exp = self.rd_exp + self.dep_exp + self.sga + self.other_op_exp
+        if self.ebit is None:
+            self.ebit = self.gross_profit - self.op_exp
+        if self.ebt is None:
+            self.ebt = self.ebit - self.int_exp
+        if self.net_income is None:
+            self.net_income = self.ebt - self.tax_exp
 
     @property
     def gross_profit(self) -> float:
         return self.revenue - self.cogs
-
-    @property
-    def ebit(self) -> float:
-        return self.gross_profit - self.op_exp
-
-    @property
-    def ebt(self) -> float:
-        return self.ebit - self.int_exp
-
-    @property
-    def net_income(self) -> float:
-        return self.ebt - self.tax_exp
 
     @property
     def effective_tax_rate(self) -> float:
