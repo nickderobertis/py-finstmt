5674c1c809b1458c067d18f063888834689bad69
Nick DeRobertis <whoopnip@gmail.com>
2019-11-20 10:13:42 -0500

    Add basic statement config manager class

---
+++ finstmt/config_manage/statement.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Sequence, Dict, Any
+
+from finstmt.items.config import ItemConfig
+
+
+@dataclass
+class StatementConfigManager:
+    configs: Sequence[ItemConfig]
+
+    def __post_init__(self):
+        self.configs = list(self.configs)
+
+    def get(self, item_key: str) -> ItemConfig:
+        """
+        Get entire configuration for item by key
+        """
+        return self.config_dict[item_key]
+
+    def set(self, item_key: str, config: ItemConfig):
+        """
+        Set entire configuration for item by key
+        """
+        orig_config = self.get(item_key)
+        config_idx = self.configs.index(orig_config)
+        self.configs[config_idx] = config
+
+    def get_value(self, item_key: str, config_key: str) -> Any:
+        """
+        Get a particular configuration for a particular item
+        """
+        config = self.get(item_key)
+        return getattr(config, config_key)
+
+    def set_value(self, item_key: str, config_key: str, value: Any):
+        """
+        Set a particular configuration for a particular item
+        """
+        orig_config = self.get(item_key)
+        setattr(orig_config, config_key, value)
+        self.set(item_key, orig_config)
+
+    @property
+    def config_dict(self) -> Dict[str, ItemConfig]:
+        return {config.key: config for config in self.configs}
