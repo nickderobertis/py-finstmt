0d573023a5ec527878c934a0a1f33a15bb8bfa76
Nick DeRobertis <whoopnip@gmail.com>
2019-12-08 17:01:11 -0500

    Add basic load tests for capital iq

--- tests/test_load.py
+++ tests/test_load.py
@@ -13,3 +13,15 @@ class TestLoadStockrow:
         inc = IncomeStatements.from_df(quarterly_stockrow_income_df)
         stmts = FinancialStatements(inc, bs)
 
+
+class TestLoadCapitalIQ:
+
+    def test_annual(self, annual_capiq_income_df, annual_capiq_bs_df):
+        bs = BalanceSheets.from_df(annual_capiq_bs_df)
+        inc = IncomeStatements.from_df(annual_capiq_income_df)
+        stmts = FinancialStatements(inc, bs)
+
+    def test_quarterly(self, quarterly_capiq_income_df, quarterly_capiq_bs_df):
+        bs = BalanceSheets.from_df(quarterly_capiq_bs_df)
+        inc = IncomeStatements.from_df(quarterly_capiq_income_df)
+        stmts = FinancialStatements(inc, bs)
