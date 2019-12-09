2515aea2eee5236f51ca4b42186a825a2aa4ab83
Nick DeRobertis <whoopnip@gmail.com>
2019-12-08 19:14:23 -0500

    Add fixtures for capiq loaded statements

--- tests/fixtures/data/capiq.py
+++ tests/fixtures/data/capiq.py
@@ -17,11 +17,29 @@ def annual_capiq_income_df() -> pd.DataFrame:
 
 
 @pytest.fixture
+def annual_capiq_income_stmt(annual_capiq_income_df) -> IncomeStatements:
+    stmt = IncomeStatements.from_df(annual_capiq_income_df)
+    return stmt
+
+
+@pytest.fixture
 def annual_capiq_bs_df() -> pd.DataFrame:
     path = os.path.join(CAPIQ_PATH, 'annual_cat.xls')
     sheet_name = 'Balance Sheet'
     df = load_capiq_df(path, sheet_name)
     return df
+
+
+@pytest.fixture
+def annual_capiq_bs_stmt(annual_capiq_bs_df) -> BalanceSheets:
+    stmt = BalanceSheets.from_df(annual_capiq_bs_df)
+    return stmt
+
+
+@pytest.fixture
+def annual_capiq_stmts(annual_capiq_income_stmt, annual_capiq_bs_stmt) -> FinancialStatements:
+    stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt)
+    return stmts
 
 
 @pytest.fixture
@@ -33,8 +51,26 @@ def quarterly_capiq_income_df() -> pd.DataFrame:
 
 
 @pytest.fixture
+def quarterly_capiq_income_stmt(quarterly_capiq_income_df) -> IncomeStatements:
+    stmt = IncomeStatements.from_df(quarterly_capiq_income_df)
+    return stmt
+
+
+@pytest.fixture
 def quarterly_capiq_bs_df() -> pd.DataFrame:
     path = os.path.join(CAPIQ_PATH, 'quarterly_cat.xls')
     sheet_name = 'Balance Sheet'
     df = load_capiq_df(path, sheet_name)
     return df
+
+
+@pytest.fixture
+def quarterly_capiq_bs_stmt(quarterly_capiq_bs_df) -> BalanceSheets:
+    stmt = BalanceSheets.from_df(quarterly_capiq_bs_df)
+    return stmt
+
+
+@pytest.fixture
+def quarterly_capiq_stmts(quarterly_capiq_income_stmt, quarterly_capiq_bs_stmt) -> FinancialStatements:
+    stmts = FinancialStatements(quarterly_capiq_income_stmt, quarterly_capiq_bs_stmt)
+    return stmts
