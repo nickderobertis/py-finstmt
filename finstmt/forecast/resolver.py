2c93a98530a8c33783043d4aeb34fc7f9f04037e
Nick DeRobertis <whoopnip@gmail.com>
2020-04-18 13:19:22 -0400

    WIP: An initial forecast resolve setup that flows all the way through, but incorrectly solves the balance sheet

--- finstmt/forecast/resolver.py
+++ finstmt/forecast/resolver.py
@@ -1,17 +1,21 @@
-from typing import TYPE_CHECKING, List, Dict, Tuple
+from typing import TYPE_CHECKING, List, Dict, Tuple, Optional
 
-from sympy import Eq, sympify, IndexedBase, Idx
+from sympy import Eq, sympify, IndexedBase, Idx, linsolve
 import pandas as pd
 from sympy.logic.boolalg import BooleanFalse, BooleanTrue
 
 from finstmt.forecast.main import Forecast
 from finstmt.config_manage.data import _key_pct_of_key
+from finstmt.forecast.statements import ForecastedFinancialStatements
+from finstmt.items.config import ItemConfig
 
 if TYPE_CHECKING:
     from finstmt.combined.statements import FinancialStatements
 
 
 class ForecastResolver:
+    solve_eqs: Optional[List[Eq]] = None
+    subs_dict: Optional[Dict[IndexedBase, float]] = None
 
     def __init__(self, stmts: 'FinancialStatements', forecast_dict: Dict[str, Forecast],
                  results: Dict[str, pd.Series]):
@@ -19,8 +23,51 @@ class ForecastResolver:
         self.forecast_dict = forecast_dict
         self.results = results
 
-    def get_solve_eqs_and_full_subs_dict(self) -> Tuple[List[Eq], Dict[IndexedBase, float]]:
-        return get_solve_eqs_and_full_subs_dict(self.all_eqs, self.sympy_subs_dict)
+        self.set_solve_eqs_and_full_subs_dict()
+
+    def set_solve_eqs_and_full_subs_dict(self):
+        solve_eqs, self.subs_dict = get_solve_eqs_and_full_subs_dict(self.all_eqs, self.sympy_subs_dict)
+        self.solve_eqs = solve_eqs + self.bs_balance_eqs
+
+    def resolve_balance_sheet(self):
+        plugs_dict = {}
+        for config in self.plug_configs:
+            plugs_dict[config.key] = self.results[config.key].values
+
+        solutions_dict = self.subs_dict.copy()
+        for key, arr in plugs_dict.items():
+            for i, val in enumerate(arr):
+                t_str = f'{key}[{i + 1}]'
+                lhs = sympify(t_str, locals=self.stmts.config.sympy_namespace)
+                solutions_dict[lhs] = val
+
+        new_solve_eqs, new_subs_dict = get_solve_eqs_and_full_subs_dict(self.solve_eqs, solutions_dict)
+        solve_exprs = []
+        to_solve_for = []
+        for eq in new_solve_eqs:
+            solve_exprs.append(eq.rhs - eq.lhs)
+            if eq.lhs not in to_solve_for:
+                to_solve_for.append(eq.lhs)
+
+        res_set = linsolve(solve_exprs, to_solve_for)
+        for all_results in res_set:
+            for sym, res in zip(to_solve_for, all_results):
+                solutions_dict[sym] = round(res, 0)
+
+        return solutions_dict
+
+    def to_statements(self) -> ForecastedFinancialStatements:
+        solutions_dict = self.resolve_balance_sheet()
+        new_results = sympy_dict_to_results_dict(solutions_dict, self.forecast_dates)
+
+        all_results = pd.concat(list(new_results.values()), axis=1).T
+        inc_df = self.stmts.income_statements.__class__.from_df(all_results)
+        bs_df = self.stmts.balance_sheets.__class__.from_df(all_results)
+
+        # type ignore added because for some reason mypy is not picking up structure
+        # correctly since it is a dataclass
+        obj = ForecastedFinancialStatements(inc_df, bs_df, self.forecast_dict)  # type: ignore
+        return obj
 
     @property
     def t_indexed_eqs(self) -> List[Eq]:
@@ -59,6 +106,10 @@ class ForecastResolver:
     def num_periods(self) -> int:
         # adding 1 because final existing period will be included as period 0
         return list(self.forecast_dict.values())[0].config.periods + 1
+
+    @property
+    def forecast_dates(self) -> pd.DatetimeIndex:
+        return list(self.results.values())[0].index
 
     @property
     def t(self) -> Idx:
@@ -111,6 +162,10 @@ class ForecastResolver:
             eqs.append(Eq(lhs, rhs))
         return eqs
 
+    @property
+    def plug_configs(self) -> List[ItemConfig]:
+        return [conf for conf in self.stmts.all_config_items if conf.forecast_config.plug]
+
 
 def get_solve_eqs_and_full_subs_dict(
     eqs_for_sub: List[Eq],
@@ -122,7 +177,7 @@ def get_solve_eqs_and_full_subs_dict(
     while not finished:
         next_eqs_to_sub = []
         for eq in eqs_for_sub:
-            subbed = eq.xreplace(subs_dict)
+            subbed = Eq(eq.lhs, eq.rhs.xreplace(subs_dict))
             if isinstance(subbed, (BooleanFalse, BooleanTrue)):
                 # Both lhs and rhs was subbed. This means it is a calculated item which was
                 # added to the forecast because it is being used as pct_of in other items
@@ -143,3 +198,17 @@ def get_solve_eqs_and_full_subs_dict(
             finished = True
         eqs_for_sub = next_eqs_to_sub.copy()
     return eqs_for_sub, subs_dict
+
+
+def sympy_dict_to_results_dict(
+    s_dict: Dict[IndexedBase, float],
+    forecast_dates: pd.DatetimeIndex
+) -> Dict[str, pd.Series]:
+    new_results = {
+        str(expr.base): pd.Series(index=forecast_dates, dtype='float', name=str(expr.base)) for expr in s_dict
+    }
+    for expr, val in s_dict.items():
+        key = str(expr.base)
+        t = int(expr.indices[0]) - 1
+        new_results[key].iloc[t] = val
+    return new_results
