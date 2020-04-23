74ea78199ab7c94ade724d3516a913dac97f8f52
Nick DeRobertis <whoopnip@gmail.com>
2020-04-23 18:24:12 -0400

    Add global config manager which manages the config which will be used for new objects

---
+++ finstmt/config_manage/global_.py
@@ -0,0 +1,18 @@
+import datetime
+
+import pandas as pd
+
+from finstmt.bs.config import BALANCE_SHEET_INPUT_ITEMS
+from finstmt.config_manage.data import DataConfigManager
+from finstmt.config_manage.statement import StatementConfigManager
+from finstmt.config_manage.statements import StatementsConfigManager
+from finstmt.inc.config import INCOME_STATEMENT_INPUT_ITEMS
+
+inc_data_config_mgr = DataConfigManager(INCOME_STATEMENT_INPUT_ITEMS)
+inc_stmt_config_mgr = StatementConfigManager({pd.Timestamp(datetime.datetime.today()): inc_data_config_mgr})
+bs_data_config_mgr = DataConfigManager(BALANCE_SHEET_INPUT_ITEMS)
+bs_stmt_config_mgr = StatementConfigManager({pd.Timestamp(datetime.datetime.today()): bs_data_config_mgr})
+CONFIG = StatementsConfigManager({
+    'income_statements': inc_stmt_config_mgr,
+    'balance_sheets': bs_stmt_config_mgr
+})
