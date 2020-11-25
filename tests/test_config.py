a9bfefb9b9acc884496f32b2aec030d38153e6a8
Nick DeRobertis <whoopnip@gmail.com>
2020-11-25 17:51:45 -0500

    Add tests for default config adjustment behavior

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,6 +16,25 @@ def check_config(config: StatementsConfigManager, json_path: Path):
     assert config.dict() == expect_data
 
 
+def test_config_load_cat_annual_stockrow(
+    annual_stockrow_income_stmt_cat: IncomeStatements,
+    annual_stockrow_bs_stmt_cat: BalanceSheets
+):
+    stmts = FinancialStatements(annual_stockrow_income_stmt_cat, annual_stockrow_bs_stmt_cat, auto_adjust_config=False)
+    json_path = EXPECT_CONFIG_PATH / 'default-cat-annual-stockrow.json'
+    check_config(stmts.config, json_path)
+
+
+def test_config_load_cat_annual_capiq(
+    annual_capiq_income_stmt: IncomeStatements,
+    annual_capiq_bs_stmt: BalanceSheets
+):
+    dates = annual_capiq_income_stmt.dates
+    stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt[dates], auto_adjust_config=False)
+    json_path = EXPECT_CONFIG_PATH / 'default-cat-annual-capiq.json'
+    check_config(stmts.config, json_path)
+
+
 def test_config_load_cat_annual_stockrow_no_adjust(
     annual_stockrow_income_stmt_cat: IncomeStatements,
     annual_stockrow_bs_stmt_cat: BalanceSheets
