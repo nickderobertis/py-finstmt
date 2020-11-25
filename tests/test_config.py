9107c2fb3acdd1e84f88350ae9d9d6c50e810438
Nick DeRobertis <whoopnip@gmail.com>
2020-11-25 17:50:44 -0500

    Convert original config tests to no adjust config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,20 +16,20 @@ def check_config(config: StatementsConfigManager, json_path: Path):
     assert config.dict() == expect_data
 
 
-def test_config_load_cat_annual_stockrow(
+def test_config_load_cat_annual_stockrow_no_adjust(
     annual_stockrow_income_stmt_cat: IncomeStatements,
     annual_stockrow_bs_stmt_cat: BalanceSheets
 ):
-    stmts = FinancialStatements(annual_stockrow_income_stmt_cat, annual_stockrow_bs_stmt_cat)
-    json_path = EXPECT_CONFIG_PATH / 'default-cat-annual-stockrow.json'
+    stmts = FinancialStatements(annual_stockrow_income_stmt_cat, annual_stockrow_bs_stmt_cat, auto_adjust_config=False)
+    json_path = EXPECT_CONFIG_PATH / 'no-adjust-cat-annual-stockrow.json'
     check_config(stmts.config, json_path)
 
 
-def test_config_load_cat_annual_capiq(
+def test_config_load_cat_annual_capiq_no_adjust(
     annual_capiq_income_stmt: IncomeStatements,
     annual_capiq_bs_stmt: BalanceSheets
 ):
     dates = annual_capiq_income_stmt.dates
-    stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt[dates])
-    json_path = EXPECT_CONFIG_PATH / 'default-cat-annual-capiq.json'
+    stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt[dates], auto_adjust_config=False)
+    json_path = EXPECT_CONFIG_PATH / 'no-adjust-cat-annual-capiq.json'
     check_config(stmts.config, json_path)
