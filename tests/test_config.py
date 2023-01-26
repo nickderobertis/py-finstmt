2830b79bbfce693cd3869a16cf1af198391f3398
Christopher Lawson <chris@lawson.net.au>
2023-01-26 11:39:08 -0600

    add test for attribute access

--- tests/test_config.py
+++ tests/test_config.py
@@ -52,3 +52,16 @@ def test_config_load_cat_annual_capiq_no_adjust(
     stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt[dates], auto_adjust_config=False)
     json_path = EXPECT_CONFIG_PATH / 'no-adjust-cat-annual-capiq.json'
     check_config(stmts.config, json_path)
+
+def test_config_attribute_access(
+    annual_capiq_income_stmt: IncomeStatements,
+    annual_capiq_bs_stmt: BalanceSheets
+):
+    dates = annual_capiq_income_stmt.dates
+    stmts = FinancialStatements(annual_capiq_income_stmt, annual_capiq_bs_stmt[dates])
+    # Check reading a config value by attribute access
+    assert stmts.config.cash.display_name == "Cash and Cash Equivalents"
+
+    # Check updating a config value by attribute access
+    stmts.config.cash.display_name = "Cash"
+    assert stmts.config.cash.display_name == "Cash"
