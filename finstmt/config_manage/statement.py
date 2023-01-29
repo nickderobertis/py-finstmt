afa76fc8b4f828b19047e64b5d00693199b65e6a
Nick DeRobertis <whoopnip@gmail.com>
2023-01-29 14:45:29 -0500

    Add config_managers check to avoid recursive loop

--- finstmt/config_manage/statement.py
+++ finstmt/config_manage/statement.py
@@ -37,6 +37,12 @@ class StatementConfigManager(ConfigManagerBase):
         """
         Get the config for a given key
         """
+        # When copying the object, config_managers can be undefined and so would cause a 
+        # recursive loop without this check
+        if item_key == "config_managers":
+            # Trigger the default Python behavior
+            return object.__getattribute__(self, item_key)
+        
         try:
             return self.get(item_key)
         except NoSuchItemException:
