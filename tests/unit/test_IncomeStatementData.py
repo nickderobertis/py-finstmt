128c4747c0105f0055bc547aeffb8c2bf1f94d76
Christopher Lawson <chris@lawson.net.au>
2023-02-17 10:46:24 -0600

    update EBITDA test

--- tests/unit/test_IncomeStatementData.py
+++ tests/unit/test_IncomeStatementData.py
@@ -4,5 +4,5 @@ def test_ebitda(annual_capiq_stmts: FinancialStatements):
     stmts = annual_capiq_stmts
 
     # Check that EBITDA = EBIT + DA
-    assert stmts.income_statements.ebitda == stmts.income_statements.ebit + stmts.income_statements.dep_exp
+    assert (stmts.income_statements.ebitda == stmts.income_statements.ebit + stmts.income_statements.dep_exp).all()
 
