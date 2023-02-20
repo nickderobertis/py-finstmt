ca7863bddf76b68f083492c623005e53d2d86176
Nick DeRobertis <whoopnip@gmail.com>
2023-02-20 15:57:28 -0500

    chore: Add tests for all operations

--- tests/unit/test_operations.py
+++ tests/unit/test_operations.py
@@ -9,3 +9,59 @@ def test_round_statement(ro_annual_capiq_stmts: FinancialStatements):
 
     rounded_decimal = round(stmts, 2)
     assert (rounded_decimal.cash == round(stmts.cash, 2)).all()
+
+
+def test_add_statements(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    added = stmts + stmts
+    assert (added.cash == stmts.cash + stmts.cash).all()
+
+
+def test_add_statements_number(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    added = stmts + 1
+    assert (added.cash == stmts.cash + 1).all()
+
+
+def test_subtract_statements(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    subtracted = stmts - stmts
+    assert (subtracted.cash == stmts.cash - stmts.cash).all()
+
+
+def test_subtract_statements_number(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    subtracted = stmts - 1
+    assert (subtracted.cash == stmts.cash - 1).all()
+
+
+def test_multiply_statements(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    multiplied = stmts * stmts
+    assert (multiplied.cash == stmts.cash * stmts.cash).all()
+
+
+def test_multiply_statements_number(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    multiplied = stmts * 2
+    assert (multiplied.cash == stmts.cash * 2).all()
+
+
+def test_divide_statements(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    divided = stmts / stmts
+    assert (divided.cash == 1).all()
+
+
+def test_divide_statements_number(ro_annual_capiq_stmts: FinancialStatements):
+    stmts = ro_annual_capiq_stmts
+
+    divided = stmts / 2
+    assert (divided.cash == stmts.cash / 2).all()
