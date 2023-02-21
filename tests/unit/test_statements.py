e069fba4066c2588b76a727b39fe0d73e068eb09
Nick DeRobertis <whoopnip@gmail.com>
2023-02-21 06:37:36 -0500

    fix: Handle calculated items in determining series names. Fixes fcf usage (#97)

--- tests/unit/test_statements.py
+++ tests/unit/test_statements.py
@@ -8,3 +8,5 @@ def test_series_name(ro_annual_capiq_stmts: FinancialStatements):
     assert stmts.inventory.name == "Inventory"
     # Check Cash and Cash Equivalents Name is set. This item includes spaces
     assert stmts.cash.name == "Cash and Cash Equivalents"
+    # Check a calculated item name is set
+    assert stmts.nwc.name == "nwc"
