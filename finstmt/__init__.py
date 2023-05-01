539f912b4e0e91474610ab6d7bffef5945a15ca8
Christopher Lawson <chris@lawson.net.au>
2023-05-01 15:37:17 -0500

    minor version bump

--- finstmt/__init__.py
+++ finstmt/__init__.py
@@ -2,6 +2,6 @@
 Work with financial statement data in Python. Can calculate free cash flows and help project
 financial statements, automatically balancing the balance sheet. 
 """
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 from finstmt.combined.statements import FinancialStatements
 from finstmt.config_manage.global_ import CONFIG
