d659d1f32238f93add2108d66bd932eff0c7fd18
Christopher Lawson <chris@lawson.net.au>
2023-05-05 14:29:33 -0500

    failing test for nested calc field

--- tests/unit/test_statement_item.py
+++ tests/unit/test_statement_item.py
@@ -14,7 +14,7 @@ def test_calculated_field():
         ),
         ItemConfig(
             key="equity",
-            display_name="equity",
+            display_name="Equity",
             expr_str="cash[t]",
         ),
     ]
@@ -32,3 +32,69 @@ def test_calculated_field():
     finstmts = FinancialStatements([stmt_timeseries])
     assert finstmts.equity[0] == 1000
 
+
+def test_nest_calculated_fields():
+    config = [
+        ItemConfig(
+            key="cash",
+            display_name="Cash",
+            extract_names=["cash"],
+        ),
+        ItemConfig(
+            key="assets",
+            display_name="Assets",
+            expr_str="cash[t]",
+        ),
+        ItemConfig(
+            key="equity",
+            display_name="Equity",
+            expr_str="assets[t]",
+        ),
+    ]
+    dict = {
+        "12/31/2022": {
+            "cash": 1000,
+        }
+    }
+
+    stmt_timeseries = FinStatementsBase.from_dict(
+        dict,
+        "Test Statment",
+        config
+    )
+    finstmts = FinancialStatements([stmt_timeseries])
+    assert finstmts.equity[0] == 1000
+
+
+# Equity is defined BEFORE Assets in config file where assets and equity are a calculated field
+def test_nested_calculated_fields_out_of_order():
+    config = [
+        ItemConfig(
+            key="cash",
+            display_name="Cash",
+            extract_names=["cash"],
+        ),
+        ItemConfig(
+            key="equity",
+            display_name="Equity",
+            expr_str="assets[t]",
+        ),
+        ItemConfig(
+            key="assets",
+            display_name="Assets",
+            expr_str="cash[t]",
+        ),
+    ]
+    dict = {
+        "12/31/2022": {
+            "cash": 1000,
+        }
+    }
+
+    stmt_timeseries = FinStatementsBase.from_dict(
+        dict,
+        "Test Statment",
+        config
+    )
+    finstmts = FinancialStatements([stmt_timeseries])
+    assert finstmts.equity[0] == 1000
