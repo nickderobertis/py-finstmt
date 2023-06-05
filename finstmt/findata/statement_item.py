b7ba6eb56dac615c83d1f217c5146c34cdf3508d
Christopher Lawson <chris@lawson.net.au>
2023-06-04 22:40:58 -0500

    new tests

--- finstmt/findata/statement_item.py
+++ finstmt/findata/statement_item.py
@@ -50,53 +50,7 @@ class StatementItem:
     # If this field is a calculated field, then update the calculated statement idem
     # This will be done by solving all calculated fields simultaneously
     def update_statement_item_calculated_value(self, statement_item_value):
-        if self.item_config.expr_str is None:
+        if self.item_config.expr_str is None: 
             return
         self.calculated_value = statement_item_value
 
-    def resolve_eq(self, date, finStmts):
-        if (
-            not self.item_config.expr_str
-        ):  # if expression string is null or empty, don't do anything
-            return
-
-        ns_syms = finStmts.global_sympy_namespace
-        sym_expr = sympify(self.item_config.expr_str, locals=ns_syms)
-        sub_list = []
-        t = ns_syms["t"]
-
-        for sym in sym_expr.free_symbols:
-            # free_symbols include everything from the provided namespace as
-            #  well as all symbols in the expression
-            # we will make an assumption that the symbols that we are actually
-            #   interested in from the provided expresison string must have an
-            #   index
-            # we will skip any items in free_symbols that are not indexed
-            if type(sym) is not Indexed:
-                continue
-            # get the series for the attribute
-            series = getattr(finStmts, str(sym.base))
-
-            # next we need to determine if the indexed symbol refers to the
-            #   current period or a different period
-            # We assume that there is only ONE index
-            idx = sym.indices[0]
-            if idx == t:
-                offset = 0
-            else:
-                offset = idx.args[0]
-
-            series_index_t0 = series.index.get_loc(date)
-            series_index_with_offset = series_index_t0 + offset
-
-            if series_index_with_offset < 0:
-                self.calculated_value = None
-                return
-
-            date_with_offset = series.index[int(series_index_with_offset)]
-            sub_value = series[date_with_offset]
-
-            sub_list.append((sym, sub_value))
-
-        result = np.float64(sym_expr.subs(sub_list))
-        self.calculated_value = result
--- tests/unit/test_statement_item.py
+++ tests/unit/test_statement_item.py
@@ -1,6 +1,74 @@
+import numpy as np
+from sympy import nan
 from finstmt.combined.statements import FinancialStatements
+from finstmt.findata.statement_item import StatementItem
 from finstmt.findata.statementsbase import FinStatementsBase
 from finstmt.items.config import ItemConfig
+
+def test_seed_value_is_none():
+    item_config = ItemConfig(
+        "cash_and_st_invest",
+        "Cash and Short-Term Investments",
+        expr_str="cash[t] + st_invest[t]",
+    )
+    statement_item = StatementItem(item_config)
+
+    assert statement_item.seed_value is None
+    assert statement_item.calculated_value is None
+    assert statement_item.value is None
+
+
+def test_update_statement_item_calculated_value_without_seed():
+    item_config = ItemConfig(
+        "cash_and_st_invest",
+        "Cash and Short-Term Investments",
+        expr_str="cash[t] + st_invest[t]",
+    )
+    statement_item = StatementItem(item_config)
+    statement_item.update_statement_item_calculated_value(11)
+
+    assert statement_item.seed_value is None
+    assert statement_item.calculated_value == 11
+    assert statement_item.value == 11
+
+
+def test_update_statement_item_calculated_value_with_seed():
+    item_config = ItemConfig(
+        "cash_and_st_invest",
+        "Cash and Short-Term Investments",
+        expr_str="cash[t] + st_invest[t]",
+    )
+    statement_item = StatementItem(item_config, 10)
+    statement_item.update_statement_item_calculated_value(11)
+
+    assert statement_item.seed_value == 10
+    assert statement_item.calculated_value == 11
+    assert statement_item.value == 10
+
+
+# These are more of an acceptance test
+def test_default_value_for_non_calculated_field():
+    config = [
+        ItemConfig(
+            key="cash",
+            display_name="Cash",
+            extract_names=["cash"],
+        ),
+        ItemConfig(
+            key="investments",
+            display_name="Investments",
+            extract_names=["Investments"],
+        ),
+    ]
+    dict = {
+        "12/31/2022": {
+            "cash": 1000
+        }
+    }
+
+    stmt_timeseries = FinStatementsBase.from_dict(dict, "Test Statment", config)
+    finstmts = FinancialStatements([stmt_timeseries])
+    assert finstmts.investments[0] == 0
 
 
 def test_calculated_field():
@@ -84,3 +152,34 @@ def test_nested_calculated_fields_out_of_order():
     stmt_timeseries = FinStatementsBase.from_dict(dict, "Test Statment", config)
     finstmts = FinancialStatements([stmt_timeseries])
     assert finstmts.equity[0] == 1000
+
+
+def test_time_shift_calculated_field():
+    config = [
+        ItemConfig(
+            key="cash",
+            display_name="Cash",
+            extract_names=["cash"],
+        ),
+        ItemConfig(
+            key="cashDelta",
+            display_name="Cash Delta",
+            expr_str="cash[t] - cash[t-1]",
+        ),
+    ]
+    dict = {
+        "12/31/2021": {
+            "cash": 1000,
+        },
+        "12/31/2022": {
+            "cash": 1100,
+        },
+    }
+
+    stmt_timeseries = FinStatementsBase.from_dict(dict, "Test Statment", config)
+    finstmts = FinancialStatements([stmt_timeseries])
+    print(type(finstmts.cashDelta[0]))
+    assert np.isnan(finstmts.cashDelta[0])
+    assert finstmts.cashDelta[1] == 100
+
+
